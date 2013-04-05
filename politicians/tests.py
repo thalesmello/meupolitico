bbcf74573c7c58793cf050e7d381e22cabef1f47
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2013-04-05 07:32:27 -0300

    Added some more unit tests

--- politicians/tests.py
+++ politicians/tests.py
@@ -1,6 +1,8 @@
+﻿# -*- coding: utf-8 -*-
+
 from django.test import TestCase
 from django.utils import timezone
-from politicians.models import Party
+from politicians.models import *
 
 class PartyModelTest(TestCase):
     def test_creating_a_new_party_and_saving_it_to_the_database(self):
@@ -21,3 +23,16 @@ class PartyModelTest(TestCase):
         # and check that it's saved its two attributes: name and acronym
         self.assertEquals(only_party_in_database.name, party.name)
         self.assertEquals(only_party_in_database.acronym, party.acronym)
+
+class PoliticianModelTest(TestCase):
+    def test_politician_objects_are_named_after_their_names(self):
+        p = Politician()
+        name = u"João"
+        p.name = name
+        self.assertEquals(name, unicode(p))
+
+class NewsModelTest(TestCase):
+    def test_verbose_name_for_pub_date(self):
+        for field in News._meta.fields:
+            if field.name ==  'pub_date':
+                self.assertEquals('date published', field.verbose_name)
