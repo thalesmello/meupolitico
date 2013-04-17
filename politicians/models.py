925c2768ec9a49f1803f55295e34b1184b74d2c0
Thales Mello <thalesmello@gmail.com>
2013-04-17 17:30:27 -0300

    Curtir noticia implementada no model

--- politicians/models.py
+++ politicians/models.py
@@ -17,11 +17,27 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     link = models.URLField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
     def __unicode__(self):
         return self.title
-		
+
+    @classmethod
+    def get_model_fields(model):
+        return model._meta.fields
+
+    def get_likes_count(self):
+        return len(self.user_set.all())
+
+    def does_user_like_me(self, user):
+        return (user in self.user_set.all())
+        
 class User(models.Model):
-	username = models.CharField(max_length=50)
-	password = models.CharField(max_length=20)
-	def __unicode__(self):
-		return self.username
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    news_liked = models.ManyToManyField(News)
+    
+    def __unicode__(self):
+        return self.username
+    
+    def like_news(self,news):
+        self.news_liked.add(news)
--- politicians/tests.py
+++ politicians/tests.py
@@ -2,6 +2,7 @@
 
 from django.test import TestCase
 from django.utils import timezone
+from datetime import date
 from politicians.models import *
 
 class PartyModelTest(TestCase):
@@ -33,6 +34,25 @@ class PoliticianModelTest(TestCase):
 
 class NewsModelTest(TestCase):
     def test_verbose_name_for_pub_date(self):
-        for field in News._meta.fields:
+        for field in News.get_model_fields():
             if field.name ==  'pub_date':
                 self.assertEquals('date published', field.verbose_name)
+
+    def test_user_likes_news(self):
+        user = User.objects.create(username="Zezinho", password="senhaDoZezinho")
+        party = Party.objects.create()
+        polit = Politician.objects.create(party=party)
+        news = News.objects.create(title="titulo inutil", politician=polit, pub_date=date.today())
+
+        #User will like news
+        user.like_news(news)
+        count_likes = news.get_likes_count()
+        self.assertEquals(count_likes, 1)
+        self.assertTrue(news.does_user_like_me(user))
+
+        #User count is still 1
+        user.like_news(news)
+        count_likes = news.get_likes_count()
+        self.assertEquals(count_likes, 1)
+
+
