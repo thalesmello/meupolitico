834cd4e48c929396f18af04c7d48ea4ab7b72934
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2013-04-18 20:47:35 -0300

    Corrected various coding problems

--- politicians/urls.py
+++ politicians/urls.py
@@ -5,17 +5,19 @@ from politicians import views
 urlpatterns = patterns('',
     # ex: /
     url(r'^$', views.home, name='home'),
+    # ex: /home/
+    url(r'^home/$', views.home, name='home'),
     # ex: /index
-    url(r'^index/$', views.index, name='index'),
+    url(r'^index/$', views.home, name='index'),
     # ex: /politicians/
     url(r'^politicians/$', views.politicians, name='politicians'),
     # ex: /politicians/1/
     url(r'^politicians/(?P<politician_id>\d+)/$', views.politician_profile, name='politician_profile'),
-	# ex: /login/
+    # ex: /login/
     url(r'^login/$', views.login, name='login'),
-	# ex: /login_user/
+    # ex: /login_user/
     url(r'^login_user/$', views.login_user, name='login_user'),
-	# ex: /logout/
+    # ex: /logout/
     url(r'^logout/$', views.logout, name='logout'),
     # ex: /news/
     url(r'^news/$', views.news, name='news'),
@@ -25,6 +27,4 @@ urlpatterns = patterns('',
     url(r'^search/$', views.search, name='search'),
     # ex: /search_results/
     url(r'^search_results/$', views.search_results, name='search_results'),
-    # ex: /home/
-    url(r'^home/$', views.home, name='home'),
 )
--- politicians/views.py
+++ politicians/views.py
@@ -3,8 +3,8 @@
 from django.shortcuts import render, get_object_or_404
 from politicians.models import Politician, News, User
 
-def index(request):
-    return render(request, 'politicians/index.html')
+def home(request):
+    return render(request, 'politicians/home.html')
 
 def politicians(request):
     politicians_list = Politician.objects.all().order_by('name')
@@ -41,6 +41,45 @@ def news(request):
     except:
         pass
 
+    news_like_count = []
+    for news in news_list:
+        news_like_count.append(news.get_likes_count())
+
+    news_like_status = []
+    for news in news_list:
+        if user_logged_in:
+            news_like_status.append(news.does_user_like_me(user))
+        else:
+            news_like_status.append(None)
+
+    news_entry = zip(news_list,news_like_count,news_like_status)
+
+    context = {'news_heading': 'Notícias Recentes', 'news_entry': news_entry}
+    return render(request, 'politicians/news.html', context)
+
+def all_news(request):
+    news_list = News.objects.all().order_by('-pub_date')
+
+    try:
+        username = request.session['username']
+        user = User.objects.get(username__exact=username)
+        user_logged_in = True
+    except KeyError, User.DoesNotExist:
+        user_logged_in = False
+
+    news_liked_id = request.POST.get('liked',None)
+    try:
+        news_liked = News.objects.get(id=news_liked_id)
+        user.like_news(news_liked)
+    except:
+        pass
+
+    news_unliked_id = request.POST.get('unliked',None)
+    try:
+        news_unliked = News.objects.get(id=news_unliked_id)
+        user.unlike_news(news_unliked)
+    except:
+        pass
 
     news_like_count = []
     for news in news_list:
@@ -54,39 +93,31 @@ def news(request):
             news_like_status.append(None)
 
     news_entry = zip(news_list,news_like_count,news_like_status)
-    
-    context = {'news_heading': 'Notícias Recentes', 'news_entry': news_entry}
-    return render(request, 'politicians/news.html', context)
 
-def all_news(request):
-    news_list = News.objects.all().order_by('-pub_date')
-    context = {'news_heading': 'Notícias', 'news_list': news_list}
+    context = {'news_heading': 'Notícias', 'news_entry': news_entry}
     return render(request, 'politicians/news.html', context)
 
 def login(request):
-	return render(request, 'politicians/login.html')
-	
+    return render(request, 'politicians/login.html')
+
 def login_user(request):
-        #FIXME: This raises an exception if you type /login_user directly
-        #in the browser
-	name = request.POST['name']
-	try:
-		user = User.objects.get(username=request.POST['name'])
-	except User.DoesNotExist:
-		context = {'message': "Usuario inexistente"}
-		return render(request, 'politicians/login.html', context)
-	if user.password == request.POST['password']:
-		request.session['username'] = user.username
-		context = {'message': "logado"}
-		return render(request, 'politicians/home.html', context)
-	else:
-		context = {'message': "Senha incorreta"}
-		return render(request, 'politicians/login.html', context)
-	
+    name = request.POST['name']
+    try:
+        user = User.objects.get(username=name)
+    except User.DoesNotExist:
+        context = {'message': "Usuario inexistente"}
+        return render(request, 'politicians/login.html', context)
+    if user.password == request.POST['password']:
+        request.session['username'] = user.username
+        return render(request, 'politicians/home.html')
+    else:
+        context = {'message': "Senha incorreta"}
+        return render(request, 'politicians/login.html', context)
+
 def logout(request):
-	del request.session['username']
-	return render(request, 'politicians/home.html')
-	
+    del request.session['username']
+    return render(request, 'politicians/home.html')
+
 def search(request):
     return render(request, 'politicians/search.html')
 
@@ -95,6 +126,3 @@ def search_results(request):
     politicians_list = Politician.objects.filter(name__contains=name)
     context = {'politicians_list': politicians_list}
     return render(request, 'politicians/politicians.html', context)
-
-def home(request):
-    return render(request, 'politicians/home.html')
