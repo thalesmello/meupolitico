4820ff7462e773dac1e4fd711250e97069ed39ed
Thales Mello <thalesmello@gmail.com>
2013-05-27 15:21:16 -0300

    modifying settings.py to work with heroku

--- meupolitico/settings.py
+++ meupolitico/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 # Django settings for meupolitico project.
 
 DEBUG = True
@@ -10,16 +11,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'meupolitico.db',                # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config()
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
