9682c8e71fe2d2a4b1ea80e6602e6aece98457f8
Thales Mello <thalesmello@gmail.com>
2013-05-27 16:20:49 -0300

    Including settings module in urls.py

--- meupolitico/urls.py
+++ meupolitico/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
