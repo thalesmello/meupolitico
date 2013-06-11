8a9f053bbcbdc6c5cccf213469f5710cebb53876
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2013-06-11 07:14:45 -0300

    All tests fixed

--- fts/tests.py
+++ fts/tests.py
@@ -4,6 +4,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+from datetime import timedelta
 
 from politicians.models import Politician, User, News
 
@@ -18,6 +19,7 @@ class PoliticiansTest(LiveServerTestCase):
         super(PoliticiansTest, cls).setUpClass()
 
     def setUp(self):
+        self.user = User.objects.all()[0]
         pass
 
     def tearDown(self):
@@ -90,29 +92,36 @@ class PoliticiansTest(LiveServerTestCase):
         # The user opens his web browser, and goes to the politicians page
         self.browser.get(self.live_server_url + '/politicians/' + str(politician.id))
 
-        # The user sees if the page contains the a heading with certain text
-        element = self.browser.find_element_by_id('heading_text')
-        self.assertIn(u'Meu Político', element.text)
-        self.assertIn(u'Perfil do Político', element.text)
-
         # The user sees if it contain all the information expected from database
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn(politician.name, body.text)
         self.assertIn(politician.party.acronym, body.text)
         self.assertIn(u'Notícias', body.text)
 
+    def execute_login(self):
+        self.browser.get(self.live_server_url+'/login/')
+        self.browser.find_element_by_id('login_username').send_keys(self.user.username)
+        self.browser.find_element_by_id('login_password').send_keys(self.user.password)
+        self.browser.find_element_by_id('login_button').click()
+
     def test_can_login(self):
         self.browser.get(self.live_server_url+'/login/')
-        self.browser.find_element_by_id('login_username').send_keys('felipe')
-        self.browser.find_element_by_id('login_password').send_keys('felipe')
-        self.browser.find_element_by_id('login_button').click()
+
         el = self.browser.find_element_by_id('login_header')
         if not el.is_displayed():
             unhide = self.browser.find_element_by_id('collapse_btn')
             unhide.click()
         el = self.browser.find_element_by_id('login_message')
-        if el.is_displayed():
-            self.assertEquals(u'Bem vindo, felipe', el.text)
+        self.assertEquals(u'Você não está logado.', el.text)
+
+        self.execute_login()
+
+        el = self.browser.find_element_by_id('login_header')
+        if not el.is_displayed():
+            unhide = self.browser.find_element_by_id('collapse_btn')
+            unhide.click()
+        el = self.browser.find_element_by_id('login_message')
+        self.assertEquals(u'Bem vindo, '+self.user.username, el.text)
 
     def test_no_like_buttons_when_not_logged(self):
         self.browser.get(self.live_server_url+'/news/')
@@ -126,27 +135,22 @@ class PoliticiansTest(LiveServerTestCase):
             assert 0
         except:
             pass
-        
+
     def test_user_sees_like_buttons(self):
-        self.browser.get(self.live_server_url+'/login/')
-        self.browser.find_element_by_id('login_username').send_keys('felipe')
-        self.browser.find_element_by_id('login_password').send_keys('felipe')
-        self.browser.find_element_by_id('login_button').click()
+        self.execute_login()
         self.browser.get(self.live_server_url+'/news/')
-        assert len(self.browser.find_elements_by_name('like_button'))==5
+        news = News.objects.all()
+        assert len(self.browser.find_elements_by_name('like_button'))==news.count()
         ct=0
         for el in self.browser.find_elements_by_name('count'):
             if "0 likes" in el.text:
                 ct=ct+1    
         assert ct==5
-        
+
     def test_user_changes_like_status(self):
-        self.browser.get(self.live_server_url+'/login/')
-        self.browser.find_element_by_id('login_username').send_keys('felipe')
-        self.browser.find_element_by_id('login_password').send_keys('felipe')
-        self.browser.find_element_by_id('login_button').click()
+        self.execute_login()
         self.browser.get(self.live_server_url+'/news/')
-        self.browser.get(self.live_server_url+'/news/')
+        news = News.objects.all()
         ct0=0
         ct1=0
         for el in self.browser.find_elements_by_name('count'):
@@ -154,7 +158,7 @@ class PoliticiansTest(LiveServerTestCase):
                 ct0=ct0+1
             elif "1 likes" in el.text:
                 ct1=ct1+1
-        assert ct0==5
+        assert ct0==news.count()
         assert ct1==0
         button = self.browser.find_elements_by_name('like_button')[0]
         button.click()
@@ -165,7 +169,7 @@ class PoliticiansTest(LiveServerTestCase):
                 ct0=ct0+1
             elif "1 likes" in el.text:
                 ct1=ct1+1
-        assert ct0==4
+        assert ct0==news.count()-1
         assert ct1==1
         pass
 
@@ -177,13 +181,21 @@ class PoliticiansTest(LiveServerTestCase):
             unhide.click()
         el = self.browser.find_element_by_id('news_search_btn')
         el.click()
+
+        found_news = []
+        for news in News.objects.all():
+            if 'critica' in news.title:
+                found_news.append(news.title)
+            news.pub_date = news.pub_date - timedelta(days=8)
+            news.save()
+
         keywords = self.browser.find_element_by_name('keywords')
-        keywords.send_keys('critica royalties')
+        keywords.send_keys('critica')
         time = self.browser.find_element_by_name('time')
         for option in time.find_elements_by_tag_name('option'):
             if option.text == 'Nesta semana':
                 option.click()
-        submit = self.browser.find_element_by_css_selector('input[type="submit"]')
+        submit = self.browser.find_element_by_id('search_news')
         submit.click()
 
         body = self.browser.find_element_by_tag_name('body')
@@ -192,25 +204,22 @@ class PoliticiansTest(LiveServerTestCase):
 
         keywords = self.browser.find_element_by_name('keywords')
         keywords.clear()
-        keywords.send_keys('critica royalties')
+        keywords.send_keys('critica')
         time = self.browser.find_element_by_name('time')
         for option in time.find_elements_by_tag_name('option'):
             if option.text == 'Qualquer data':
                 option.click()
-        submit = self.browser.find_element_by_css_selector('input[type="submit"]')
+        submit = self.browser.find_element_by_id('search_news')
         submit.click()
         body = self.browser.find_element_by_tag_name('body')
-        self.assertIn(u'critica Secretaria da Micro', body.text)
-        self.assertIn(u'Dilma detalha vetos', body.text)
+
+        for news in found_news:
+            self.assertIn(news, body.text)
 
     def test_user_changes_tendencioso_status(self):
-        self.browser.get(self.live_server_url+'/login/')
-        self.browser.find_element_by_id('login_username').send_keys('caio')
-        self.browser.find_element_by_id('login_password').send_keys('caio')
-        self.browser.find_element_by_id('login_button').click()
+        self.execute_login()
+
         self.browser.get(self.live_server_url+'/news/')
-        self.browser.get(self.live_server_url+'/news/')
-        
         ctZero = 0
         ct = 0
         for el in self.browser.find_elements_by_name('rating'):
